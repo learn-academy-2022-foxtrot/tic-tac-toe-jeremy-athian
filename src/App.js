6bf9f2e8b8ab529fa31b2aa425984ba5384672a7
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-26 15:09:40 -0700

    As a user, I can't click on a square that has already been marked

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import './App.css'
 
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
+  const [playerOneTurn, setPlayerOneTurn] = useState(true)
+
+  // every click triggers onclick which triggers the block of code below that 
+  // extends to line 23
   const selectSquare = (index) => {
+    // ... is spread operator
     const updateSquare = [...squares]
-    updateSquare[index] = "x"
+    if(squares[index] === null) {
+      if(playerOneTurn){
+        updateSquare[index] = "X"
+        setPlayerOneTurn(false)
+      } else {
+        updateSquare[index] = "O"
+        setPlayerOneTurn(true)
+      }
+    }
+   
+    
     setSquares(updateSquare)
   }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Square = ({selectSquare, index, space, key}) => {
+
   const handleClick = () => {
     selectSquare(index)
   } 
+  
   return (
     <div className="square" onClick={()=>handleClick()}>
       {space}
